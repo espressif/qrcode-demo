00e5a1e98e6853ba9b9f495b2d3ee2a2fbcc26dc
Dmitry <Dmitry.Yakovlev@espressif.com>
2022-04-28 07:34:03 +0300

    First commit with working application without opencv

---
+++ lib/quirc_opencv.cpp
@@ -0,0 +1,202 @@
+/*
+ * SPDX-FileCopyrightText: 2017-2021 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+//
+#include <stdio.h>
+#include <string.h>
+
+#include <iostream>
+#include <unistd.h>
+#include "quirc.h"
+#include "quirc_opencv.h"
+#include "quirc_internal.h"
+
+static const char *TAG = "QRcode-opencv";
+#include "esp_log.h"
+
+quirc_opencv::quirc_opencv() : matrix(3, 3), point(3, 1), point_(1, 3, CV_32F), matrix_(3, 3, CV_32F)
+{
+}
+
+quirc_opencv::~quirc_opencv()
+{
+}
+
+void quirc_opencv::warpPerspective(uint8_t *src, cv::Size src_size, uint8_t *dst, cv::Size dst_size, cv::Mat &M)
+{
+    // Clear initial data
+    matrix *= 0;
+    point *= 0;
+
+    for (int x = 0; x < 3; x++)
+    {
+        for (int y = 0; y < 3; y++)
+        {
+            matrix(y, x) = M.at<double>(y, x);
+        }
+    }
+    matrix = matrix.inverse();
+
+    for (int y = 0; y < dst_size.height; y++)
+    {
+        // std::cout << "my_warpPerspective1 y = " << y << std::endl;
+        for (int x = 0; x < dst_size.width; x++)
+        {
+            point(0, 0) = x;
+            point(1, 0) = y;
+            point(2, 0) = 1;
+            dspm::Mat aaa = matrix * point;
+            float rest = 1 / aaa(2, 0);
+            uint16_t x__ = rint(aaa(0, 0) * rest);
+            uint16_t y__ = rint(aaa(1, 0) * rest);
+
+            if ((x__ < src_size.width) &&
+                (y__ < src_size.height))
+            {
+                dst[y * dst_size.width + x] = src[y__ * src_size.width + x__];
+            }
+        }
+    }
+}
+
+bool quirc_opencv::ConvertImage(struct quirc *qr)
+{
+    if (qr->num_capstones < 3)
+        return false;
+
+    struct quirc_grid *gr = &qr->grids[0];
+
+    struct quirc_capstone *a = &(qr->capstones[gr->caps[0]]);
+    struct quirc_capstone *b = &(qr->capstones[gr->caps[1]]);
+    struct quirc_capstone *c = &(qr->capstones[gr->caps[2]]);
+
+    float dx = 7;
+    float dw = qr->grid_size - dx;
+    points_src.clear();
+    points_dest.clear();
+
+    if (qr->align_point.x != 0)
+    {
+        // center a
+        points_src.push_back(cv::Point2f(a->center.x, a->center.y));
+        points_dest.push_back(cv::Point2f(3, 3 + dw));
+        // center b
+        points_src.push_back(cv::Point2f(b->center.x, b->center.y));
+        points_dest.push_back(cv::Point2f(3, 3));
+        // center c
+        points_src.push_back(cv::Point2f(c->center.x, c->center.y));
+        points_dest.push_back(cv::Point2f(3 + dw, 3));
+
+        points_src.push_back(cv::Point2f(qr->align_point.x, qr->align_point.y));
+        points_dest.push_back(cv::Point2f(-0.5 + dw, -0.5 + dw));
+    }
+    else
+    {
+        // point a
+        points_src.push_back(cv::Point2f(a->corners[0].x, a->corners[0].y));
+        points_dest.push_back(cv::Point2f(-0.5, -0.5 + dw));
+        points_src.push_back(cv::Point2f(a->corners[1].x, a->corners[1].y));
+        points_dest.push_back(cv::Point2f(-0.5 + dx, -0.5 + dw));
+        points_src.push_back(cv::Point2f(a->corners[2].x, a->corners[2].y));
+        points_dest.push_back(cv::Point2f(-0.5 + dx, -0.5 + dx + dw));
+        points_src.push_back(cv::Point2f(a->corners[3].x, a->corners[3].y));
+        points_dest.push_back(cv::Point2f(-0.5, -0.5 + dx + dw));
+        // point b
+        points_src.push_back(cv::Point2f(b->corners[0].x, b->corners[0].y));
+        points_dest.push_back(cv::Point2f(-0.5, -0.5));
+        points_src.push_back(cv::Point2f(b->corners[1].x, b->corners[1].y));
+        points_dest.push_back(cv::Point2f(-0.5 + dx, -0.5));
+        points_src.push_back(cv::Point2f(b->corners[2].x, b->corners[2].y));
+        points_dest.push_back(cv::Point2f(-0.5 + dx, -0.5 + dx));
+        points_src.push_back(cv::Point2f(b->corners[3].x, b->corners[3].y));
+        points_dest.push_back(cv::Point2f(-0.5, -0.5 + dx));
+        // point c
+        points_src.push_back(cv::Point2f(c->corners[0].x, c->corners[0].y));
+        points_dest.push_back(cv::Point2f(-0.5 + dw, -0.5));
+        points_src.push_back(cv::Point2f(c->corners[1].x, c->corners[1].y));
+        points_dest.push_back(cv::Point2f(-0.5 + dx + dw, -0.5));
+        points_src.push_back(cv::Point2f(c->corners[2].x, c->corners[2].y));
+        points_dest.push_back(cv::Point2f(-0.5 + dx + dw, -0.5 + dx));
+        points_src.push_back(cv::Point2f(c->corners[3].x, c->corners[3].y));
+        points_dest.push_back(cv::Point2f(-0.5 + +dw, -0.5 + dx));
+    }
+
+    ESP_LOGD(TAG, "Grid size = %i, qr->h=%i, qr->w=%i", qr->grid_size, qr->h, qr->w);
+	cv::Mat temp;
+    cv::Mat H12 = cv::findHomography(points_src, points_dest, cv::RANSAC, 3, temp, 20, 0.999); 
+    float diff = 0;
+    {
+        if (qr->align_point.x != 0)
+        {
+            // point a
+            points_src.push_back(cv::Point2f(a->corners[0].x, a->corners[0].y));
+            points_dest.push_back(cv::Point2f(-0.5, -0.5 + dw));
+            points_src.push_back(cv::Point2f(a->corners[1].x, a->corners[1].y));
+            points_dest.push_back(cv::Point2f(-0.5 + dx, -0.5 + dw));
+            points_src.push_back(cv::Point2f(a->corners[2].x, a->corners[2].y));
+            points_dest.push_back(cv::Point2f(-0.5 + dx, -0.5 + dx + dw));
+            points_src.push_back(cv::Point2f(a->corners[3].x, a->corners[3].y));
+            points_dest.push_back(cv::Point2f(-0.5, -0.5 + dx + dw));
+            // point b
+            points_src.push_back(cv::Point2f(b->corners[0].x, b->corners[0].y));
+            points_dest.push_back(cv::Point2f(-0.5, -0.5));
+            points_src.push_back(cv::Point2f(b->corners[1].x, b->corners[1].y));
+            points_dest.push_back(cv::Point2f(-0.5 + dx, -0.5));
+            points_src.push_back(cv::Point2f(b->corners[2].x, b->corners[2].y));
+            points_dest.push_back(cv::Point2f(-0.5 + dx, -0.5 + dx));
+            points_src.push_back(cv::Point2f(b->corners[3].x, b->corners[3].y));
+            points_dest.push_back(cv::Point2f(-0.5, -0.5 + dx));
+            // point c
+            points_src.push_back(cv::Point2f(c->corners[0].x, c->corners[0].y));
+            points_dest.push_back(cv::Point2f(-0.5 + dw, -0.5));
+            points_src.push_back(cv::Point2f(c->corners[1].x, c->corners[1].y));
+            points_dest.push_back(cv::Point2f(-0.5 + dx + dw, -0.5));
+            points_src.push_back(cv::Point2f(c->corners[2].x, c->corners[2].y));
+            points_dest.push_back(cv::Point2f(-0.5 + dx + dw, -0.5 + dx));
+            points_src.push_back(cv::Point2f(c->corners[3].x, c->corners[3].y));
+            points_dest.push_back(cv::Point2f(-0.5 + +dw, -0.5 + dx));
+        }
+        H12.convertTo(matrix_, matrix_.type());
+        for (int i = 0; i < points_src.size(); i++)
+        {
+            point_.at<float>(0, 0) = points_src[i].x;
+            point_.at<float>(0, 1) = points_src[i].y;
+            point_.at<float>(0, 2) = 1;
+            cv::Mat aaa = matrix_ * point_.t();
+            float x__ = aaa.at<float>(0, 0) / aaa.at<float>(2, 0);
+            float y__ = aaa.at<float>(1, 0) / aaa.at<float>(2, 0);
+            diff += abs(x__ - points_dest[i].x);
+            diff += abs(y__ - points_dest[i].y);
+        }
+        ESP_LOGD(TAG,"Diff = %f" , diff);
+
+        // Check if the error is low
+        if (diff > 8)
+            return false;
+    }
+
+    warpPerspective(qr->image, cv::Size(qr->w, qr->h) , qr->pixels, cv::Size(qr->grid_size, qr->grid_size), H12);
+
+    uint8_t threshold = otsu_pic(qr->pixels, qr->grid_size, qr->grid_size, 0, 0, qr->grid_size, qr->grid_size);
+
+    ESP_LOGD(TAG, "==>Final threshold = %i", threshold);
+
+    for (size_t y = 0; y < qr->grid_size; y++)
+    {
+        for (int x = 0; x < qr->grid_size; x++)
+        {
+            uint8_t av_val = qr->pixels[y*qr->grid_size + x];
+            if (av_val <= threshold)
+            {
+                qr->pixels[qr->grid_size * y + x] = 0x01;
+            }
+            else
+            {
+                qr->pixels[qr->grid_size * y + x] = 0x00;
+            }
+        }
+    }
+    return true;
+}
---
+++ main/camera/who_camera.c
@@ -0,0 +1,106 @@
+#include "who_camera.h"
+
+#include "esp_log.h"
+#include "esp_system.h"
+
+static const char *TAG = "who_camera";
+static QueueHandle_t xQueueFrameO1 = NULL;
+static QueueHandle_t xQueueFrameO2 = NULL;
+static uint8_t frame_count = 0;
+static camera_config_t config;
+
+static void task_process_handler(void *arg)
+{
+    while (true)
+    {
+        camera_fb_t *frame = esp_camera_fb_get();
+        if (frame)
+        {
+            frame_count++;
+            if ((frame_count&1) == 1)
+            {
+                xQueueSend(xQueueFrameO1, &frame, portMAX_DELAY);
+            } else
+            {
+                xQueueSend(xQueueFrameO2, &frame, portMAX_DELAY);
+            }
+        }
+    }
+}
+
+void init_camera()
+{
+
+}
+void register_camera(const pixformat_t pixel_fromat,
+                     const framesize_t frame_size,
+                     const uint8_t fb_count,
+                     const QueueHandle_t frame_o1,
+                     const QueueHandle_t frame_o2)
+{
+    ESP_LOGI(TAG, "Camera module is %s", CAMERA_MODULE_NAME);
+
+#if CONFIG_CAMERA_MODULE_ESP_EYE || CONFIG_CAMERA_MODULE_ESP32_CAM_BOARD
+    /* IO13, IO14 is designed for JTAG by default,
+     * to use it as generalized input,
+     * firstly declair it as pullup input */
+    gpio_config_t conf;
+    conf.mode = GPIO_MODE_INPUT;
+    conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    conf.intr_type = GPIO_INTR_DISABLE;
+    conf.pin_bit_mask = 1LL << 13;
+    gpio_config(&conf);
+    conf.pin_bit_mask = 1LL << 14;
+    gpio_config(&conf);
+#endif
+
+    config.ledc_channel = LEDC_CHANNEL_0;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = CAMERA_PIN_D0;
+    config.pin_d1 = CAMERA_PIN_D1;
+    config.pin_d2 = CAMERA_PIN_D2;
+    config.pin_d3 = CAMERA_PIN_D3;
+    config.pin_d4 = CAMERA_PIN_D4;
+    config.pin_d5 = CAMERA_PIN_D5;
+    config.pin_d6 = CAMERA_PIN_D6;
+    config.pin_d7 = CAMERA_PIN_D7;
+    config.pin_xclk = CAMERA_PIN_XCLK;
+    config.pin_pclk = CAMERA_PIN_PCLK;
+    config.pin_vsync = CAMERA_PIN_VSYNC;
+    config.pin_href = CAMERA_PIN_HREF;
+    config.pin_sscb_sda = CAMERA_PIN_SIOD;
+    config.pin_sscb_scl = CAMERA_PIN_SIOC;
+    config.pin_pwdn = CAMERA_PIN_PWDN;
+    config.pin_reset = CAMERA_PIN_RESET;
+    config.xclk_freq_hz = XCLK_FREQ_HZ;
+    config.pixel_format = pixel_fromat;
+    config.frame_size = frame_size;
+    config.jpeg_quality = 0;
+    config.fb_count = fb_count;
+    config.fb_location = CAMERA_FB_IN_PSRAM;
+    config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+
+    // camera init
+    esp_err_t err = esp_camera_init(&config);
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Camera init failed with error 0x%x", err);
+        return;
+    }
+
+    sensor_t *s = esp_camera_sensor_get();
+    ESP_LOGI(TAG, "Camera sensor %2.2x %2.2x %4.4x %2.2x", s->id.MIDH, s->id.MIDL, s->id.PID, s->id.VER);
+
+    //initial sensors are flipped vertically and colors are a bit saturated
+    if (s->id.PID == OV3660_PID)
+    {
+        s->set_brightness(s, 1);  //up the blightness just a bit
+        s->set_saturation(s, -2); //lower the saturation
+    }
+    // s->set_gain_ctrl(s, 0);
+
+    xQueueFrameO1 = frame_o1;
+    xQueueFrameO2 = frame_o2;
+    xTaskCreatePinnedToCore(task_process_handler, TAG, 8 * 1024, NULL, 5, NULL, 1);
+}
