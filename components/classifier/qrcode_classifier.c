2a0e6b33570d22e1f21961851dcb0ff887081ae9
Ivan Grokhotkov <ivan@espressif.com>
2023-06-09 21:33:14 +0200

    code comments, formatting

--- components/classifier/qrcode_classifier.c
+++ components/classifier/qrcode_classifier.c
@@ -1,10 +1,15 @@
-#include "qrcode_classifier.h"
-#include "sys/types.h"
-#include <regex.h>
-#include "esp_log.h"
+/*
+ * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 #include <stdio.h>
 #include <string.h>
+#include <regex.h>
 #include <sys/queue.h>
+#include <sys/types.h>
+#include "esp_log.h"
+#include "qrcode_classifier.h"
 
 static const char *TAG = "classifier";
 
@@ -17,26 +22,27 @@ struct classifier {
 
 static TAILQ_HEAD(classifier_list, classifier) s_classifiers = TAILQ_HEAD_INITIALIZER(s_classifiers);
 
-void classifier_init(void)
+void classifier_init(const char *config_path)
 {
-    FILE* in = fopen("/data/qrclass.txt", "r");
+    FILE *in = fopen(config_path, "r");
     if (in == NULL) {
-        ESP_LOGE(TAG, "Failed to open /data/qrclass.txt. No classifiers loaded.");
+        ESP_LOGE(TAG, "Failed to open %s. No classifiers loaded.", config_path);
         return;
     }
-    while(!feof(in)) {
+
+    while (!feof(in)) {
         regex_t re;
         char regex[128];
         char filename[128];
 
         fscanf(in, "%s %s\n", regex, filename);
         ESP_LOGI(TAG, "Compiling regex '%s' for filename '%s'", regex, filename);
-        if (regcomp(&re, regex, REG_EXTENDED|REG_NOSUB) != 0) {
+        if (regcomp(&re, regex, REG_EXTENDED | REG_NOSUB) != 0) {
             ESP_LOGE(TAG, "Failed to compile regex '%s'", regex);
             continue;
         }
 
-        struct classifier* pcls = calloc(sizeof(*pcls), 1);
+        struct classifier *pcls = calloc(sizeof(*pcls), 1);
         assert(pcls);
         pcls->re = re;
         strlcpy(pcls->filename, filename, sizeof(pcls->filename));
@@ -46,7 +52,7 @@ void classifier_init(void)
     fclose(in);
 }
 
-const char* classifier_get_pic_from_qrcode_data(const char* text)
+const char *classifier_get_pic_from_qrcode_data(const char *text)
 {
     struct classifier *it;
     TAILQ_FOREACH(it, &s_classifiers, list_entry) {
@@ -55,6 +61,6 @@ const char* classifier_get_pic_from_qrcode_data(const char* text)
             return it->filename;
         }
     }
-    
+
     return NULL;
 }
